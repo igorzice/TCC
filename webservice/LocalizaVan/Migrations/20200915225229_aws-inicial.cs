8e931dec2f087266f76cba1167cfed4707b709d5
Luiz Fernando Nunes <luizfernando.fepi@gmail.com>
2020-09-15 20:10:33 -0300

    atualizando referência para banco de dados na nuvem aws

---
+++ webservice/LocalizaVan/Migrations/20200915225229_aws-inicial.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace LocalizaVan.Migrations
+{
+    public partial class awsinicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Itinerarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Itinerarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Motorista = table.Column<string>(nullable: true),
+                    Capacidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Localizacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    DataHora = table.Column<DateTime>(nullable: false),
+                    VeiculoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Localizacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Localizacoes_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Viagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Inicio = table.Column<DateTime>(nullable: false),
+                    Fim = table.Column<DateTime>(nullable: false),
+                    Finalizada = table.Column<bool>(nullable: false),
+                    VeiculoId = table.Column<int>(nullable: false),
+                    ItinerarioId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Viagens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Viagens_Itinerarios_ItinerarioId",
+                        column: x => x.ItinerarioId,
+                        principalTable: "Itinerarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Viagens_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Localizacoes_VeiculoId",
+                table: "Localizacoes",
+                column: "VeiculoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Viagens_ItinerarioId",
+                table: "Viagens",
+                column: "ItinerarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Viagens_VeiculoId",
+                table: "Viagens",
+                column: "VeiculoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Localizacoes");
+
+            migrationBuilder.DropTable(
+                name: "Viagens");
+
+            migrationBuilder.DropTable(
+                name: "Itinerarios");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
---
+++ webservice/LocalizaVan/README.md
@@ -0,0 +1,5 @@
+# Localiza Veiculo
+
+Este projeto consiste em uma aplicação para localização de veículos. 
+O projeto contém a uma API Web RESTFull para gerenciar dados de geolocalização de veículos.
+Os dados de geolocalização são obtidos usando Arduino.
